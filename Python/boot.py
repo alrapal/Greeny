c6a05ed913699c39801e381ef643f5c986b6580c
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2023-06-14 15:41:56 +0200

    Added WIFI connection in the boot.py + wifi module with 2 methods from template. Secrets.py added to gitignore

--- Python/boot.py
+++ Python/boot.py
@@ -1,4 +1,14 @@
 # boot.py -- run on boot-up
-import main
+from WIFI import do_connect, http_get
 
-main()
+# WiFi Connection
+try:
+    ip = do_connect()
+except KeyboardInterrupt:
+    print("Keyboard interrupt")
+
+# HTTP request
+try:
+    http_get()
+except Exception as err:
+    print("Exception", err)
